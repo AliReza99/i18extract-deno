689b04c85094827a38602ab21e8459a077ed7c3c
Alireza Bahrani <alirezawbhr@gmail.com>
2024-10-14 11:04:36 +0330

    refactor: improve readability

--- src/cli.ts
+++ src/cli.ts
@@ -9,12 +9,8 @@ import { translateLocalesFiles } from './translateEmptyTranslations';
 import { checkMissingTranslations } from './checkMissingTranslations';
 import { to } from 'await-to-js';
 
-const program = new Command();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const fallbackConfigFilePath = path.resolve(__dirname, 'i18next-parser.config.js');
 
 const currentDir = process.cwd();
 
@@ -25,7 +21,9 @@ function cleanup() {
   fse.removeSync(`${currentDir}/i18next-parser.config.js`);
 }
 
-async function main() {
+const program = new Command();
+
+function getCliOptions() {
   program
     .requiredOption(
       '-l, --locales <input>',
@@ -36,39 +34,69 @@ async function main() {
     .requiredOption('-o, --output <input>', 'locales output directory like: src/locales')
     .parse(process.argv);
 
-  const options = program.opts<{
+  return program.opts<{
     locales: string;
     output: string;
     input: string;
     lint?: boolean;
   }>();
+}
 
-  const locales = options.locales.split(',');
-  const defaultLocale = locales[0];
-  const localesOutputDir = path.resolve(options.output);
-
+function ensurei18nextParserConfigNotExist() {
   const haveConfig =
     fse.pathExistsSync(path.resolve(currentDir, 'i18next-parser.config.ts')) ||
     fse.pathExistsSync(path.resolve(currentDir, 'i18next-parser.config.js'));
 
   if (haveConfig) {
-    console.log('existing i18next-parser config found. exiting...');
-    return;
+    throw new Error(
+      `existing i18next-parser config found which will be overwritten. remove and run the command again. exiting...`,
+    );
   }
+}
+
+function generateConfigFileContent({
+  outputDir,
+  input,
+  defaultLocale,
+  locales,
+}: {
+  outputDir: string;
+  input: string;
+  defaultLocale: string;
+  locales: string[];
+}) {
+  const i18parserOutput = `${outputDir}/$LOCALE.json`;
+  const fallbackConfigFilePath = path.resolve(__dirname, 'i18next-parser.config.js');
+
+  return fse
+    .readFileSync(fallbackConfigFilePath, 'utf-8')
+    .replace('___OUTPUT___', i18parserOutput)
+    .replace('___INPUT___', input)
+    .replace('___DEFAULT_LOCALE___', defaultLocale)
+    .replace('___LOCALES___', JSON.stringify(locales));
+}
+
+async function main() {
+  const options = getCliOptions();
+
+  const locales = options.locales.split(',');
+  const defaultLocale = locales[0];
+  const localesOutputDir = path.resolve(options.output);
+
+  ensurei18nextParserConfigNotExist();
 
   const outputDir = options.lint ? `${currentDir}/node_modules/.tmp/i18linter` : options.output;
 
-  const i18parserOutput = `${outputDir}/$LOCALE.json`;
-
-  let configFileContent = fse
-    .readFileSync(fallbackConfigFilePath, 'utf-8')
-    .replace('___OUTPUT___', i18parserOutput)
-    .replace('___INPUT___', options.input)
-    .replace('___DEFAULT_LOCALE___', defaultLocale)
-    .replace('___LOCALES___', JSON.stringify(locales));
+  const configFileContent = generateConfigFileContent({
+    input: options.input,
+    defaultLocale,
+    outputDir,
+    locales,
+  });
 
   fse.outputFileSync(`${currentDir}/i18next-parser.config.js`, configFileContent);
   configFileCreated = true;
+
   const localesToTranslate = locales.filter(localeName => localeName !== defaultLocale);
 
   try {
