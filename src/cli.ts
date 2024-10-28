1dca4a431f98a50709795c0dc0ed9655a6272310
AliReza99 <alirezawbhr@gmail.com>
2024-10-28 09:23:27 +0330

    feat: exit with error code

--- src/cli.ts
+++ src/cli.ts
@@ -139,7 +139,7 @@ async function main() {
       console.error(err.message);
     }
     console.log('[info] run the command without "--lint" to add missing translations');
-    throw err;
+    process.exit(0);
   } finally {
     cleanup();
   }
